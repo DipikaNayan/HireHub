fc0bb757503e2005880213424d725092839c2db5
Dipika <dipikanayan100@gmail.com>
2026-07-20 12:24:40 +0530

    Implemented user registration with validation and password hashing

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,3 +7,18 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("✅ Connected to DB successfully");
+//   } catch (error) {
+//     console.error("❌ Database connection failed:");
+//     console.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require("../models/user.js");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+const register = async (req, res) => {
+  try {
+    const { fullName, email, password, role } = req.body;
+
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        message: "Invalid email address",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already in use",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+module.exports = {
+  register,
+};
